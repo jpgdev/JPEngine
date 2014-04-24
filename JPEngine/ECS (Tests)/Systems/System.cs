65781bd062130533c7c89c6ea8fe5c30d59015f1
jpdev <jp.goulet17@gmail.com>
2014-04-23 23:08:23 -0400

    Started some Entity Component System tests.

---
+++ JPEngine/ECS (Tests)/Systems/System.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPEngine.ECS
+{
+    internal abstract class ISystem
+    {
+        public abstract void Update(GameTime gameTime, EntityManager manager);
+    }
+}
